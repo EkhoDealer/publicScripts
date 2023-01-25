4b80fc38eb5feb90aea013a6a9f3fd552edb2051
Rowan Mockler <rowanmockler@gmail.com>
2023-01-25 11:43:03 -0500

    Added the correct CSS link

--- ekhoDealerCheckoutButton.js
+++ ekhoDealerCheckoutButton.js
@@ -705,7 +705,9 @@ EkhoDealerButton.prototype.loadButtonCss = function() {
   var link = document.createElement('link');
   link.type = 'text/css'
   link.rel = 'stylesheet'
-  link.href = this.baseUrl + '/css/partially-button.css';
+  // link.href = this.baseUrl + '/css/partially-button.css';
+  link.href = 'https://ekhodealer.github.io/publicScripts/ekhoDealerCheckoutButton.css'
+
   head.appendChild(link);
 }
 
